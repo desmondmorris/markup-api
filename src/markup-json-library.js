30ec5bb51cb43fb525a53003a2368e8a1869f8db
M. Maxwell Watson <hi@mmwtsn.com>
2014-10-01 13:46:50 -0400

    extracts markdown jsonp library to separate file

---
+++ src/markup-json-library.js
@@ -0,0 +1,15 @@
+(function ($) {
+  'use strict';
+
+  function insertStyles(styles) {
+    var open  = '<style type="text/css" media="all">';
+    var close = '</style>';
+
+    return open + styles + close;
+  }
+
+  function render(data) {
+    $('head').append(insertStyles(data.css));
+    $('#target').append(data.html);
+  }
+}(jQuery));
